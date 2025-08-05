2b10f685dfef75da4f3c8b1515b3e94cd8af62c8
zoya <cse.zoya.2003@gmail.com>
2025-08-05 21:50:34 +0530

    three js

--- Three_js_vite/main.js
+++ Three_js_vite/main.js
@@ -1,37 +1,40 @@
-import * as THREE from 'three';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
-const scene = new THREE.Scene()
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, .1, 1000);
+import * as THREE from 'three'
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import {RGBELoader} from 'three/examples/jsm/loaders/RGBELoader.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
-camera.position.z = 5
-
-// let geometry = new THREE.SphereGeometry(1, 5, 5, 2, 1.2)
-let geometry = new THREE.SphereGeometry(1, 10, 10)
-let material = new THREE.MeshBasicMaterial({color: "red", wireframe: true})
-let sphere = new THREE.Mesh(geometry, material)
-scene.add(sphere)
+let scene = new THREE.Scene()
+let camera = new  THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, .1, 1000);
+camera.position.set(0,0,6)
 
 const canvas = document.querySelector('#draw')
-let renderer = new THREE.WebGLRenderer({canvas: canvas, antialias: true})
-
+let renderer = new THREE.WebGLRenderer({canvas:canvas, antialias:true})
 renderer.setSize(window.innerWidth, window.innerHeight)
+renderer.setPixelRatio(window.devicePixelRatio)
+renderer.toneMapping = THREE.ACESFilmicToneMapping;
+renderer.outputEncoding = THREE.sRGBEncoding;
 
-window.addEventListener('resize', ()=>{
-  renderer.setSize(window.innerWidth, window.innerHeight)
-  camera.aspect = window.innerWidth/window.innerHeight
-  camera.updateProjectionMatrix()
+const rgbeLoader = new RGBELoader()
+rgbeLoader.load('https://dl.polyhaven.org/file/ph-assets/HDRIs/hdr/1k/zawiszy_czarnego_1k.hdr', function(texture){
+  texture.mapping = THREE.EquirectangularReflectionMapping;
+  scene.environment = texture;
+  // scene.background = texture;
+})
+
+const loader = new GLTFLoader();
+loader.load('./wooden_box (1).glb', function(gltf){
+  gltf.scene.position.y = -1.2
+  scene.add(gltf.scene)
 })
 
 let controls = new OrbitControls(camera, renderer.domElement)
-controls.autoRotate = true
 controls.enableDamping = true
+controls.dampingFactor = 0.25
 controls.enableZoom = true
 
-function animate(){
+function animate() {
   window.requestAnimationFrame(animate)
-  sphere.rotation.x += 0.01
-  sphere.rotation.y += 0.01
-  renderer.render(scene, camera)
+  renderer.render(scene, camera);
   controls.update()
 }
 animate()
