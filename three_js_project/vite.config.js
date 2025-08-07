a350ab693bdd2a7e22ba487d8f19e51b151b10df
zoya <cse.zoya.2003@gmail.com>
2025-08-07 22:20:08 +0530

    Re-added three_js_project as normal folder

---
+++ three_js_project/vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+import glsl from 'vite-plugin-glsl';
+
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+    glsl({
+    include: ['**/*.glsl', '**/*.vs', '**/*.fs'], // default
+    })
+  ],
+})
+
